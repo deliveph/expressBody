b3a0b93cbe9ef6c4daf5eb2ffb0dd0094accbabd
黄惠威 <hhw_menory@yeah.net>
2017-10-11 01:49:55 +0800

    1修复用户端订单列表显示问题，2修复收单详情页面类型logo显示问题

--- src/components/user/finish.vue
+++ src/components/user/finish.vue
@@ -1,7 +1,9 @@
 <template>
     <div class="wrapper">
         <tab :line-width=2 v-model="index">
-            <tab-item class="vux-center" v-for="(item, index) in list" :key="index" @on-item-click="order(index)"><span>{{item.name}}({{item.num}})</span></tab-item>
+            <tab-item class="vux-center" v-for="(item, index) in list" :key="index" @on-item-click="order(index)">
+                <span>{{item.name}}({{item.num}})</span>
+            </tab-item>
         </tab>
         <div class="order-container" v-model="index">
             <div class="order-con-box" v-if="index == 0">
@@ -31,15 +33,15 @@
                                                             <p>{{ship.consignee_name}}</p>
                                                         </div>
                                                     </div>
-                                                    
+
                                                 </div>
                                             </router-link>
                                             <div class="order-option">
-                                                <span  class="order-btn" v-if="ship.ship_order_status_id == -1">删除订单</span>
-                                                <span  class="order-btn" v-else-if="ship.ship_order_status_id == 1 || ship.ship_order_status_id == 2">取消订单</span>
-                                                <span  class="order-btn bd-finish" v-else-if="ship.ship_order_status_id == 3">去支付</span>
-                                                <span  class="order-btn bd-finish" v-else-if="ship.ship_order_status_id == 4">去评价</span>
-                                                <span  class="order-btn" v-else-if="ship.ship_order_status_id == 5">删除订单</span>
+                                                <span class="order-btn" v-if="ship.ship_order_status_id == -1">删除订单</span>
+                                                <span class="order-btn" v-else-if="ship.ship_order_status_id == 1 || ship.ship_order_status_id == 2">取消订单</span>
+                                                <span class="order-btn bd-finish" v-else-if="ship.ship_order_status_id == 3">去支付</span>
+                                                <span class="order-btn bd-finish" v-else-if="ship.ship_order_status_id == 4">去评价</span>
+                                                <span class="order-btn" v-else-if="ship.ship_order_status_id == 5">删除订单</span>
                                             </div>
                                         </li>
                                     </div>
@@ -64,17 +66,21 @@
                                             <router-link :to="{path:'repget',query:{ship_order_number:collection.collection_order_number,status:'user'}}">
                                                 <i class="icon_put"></i>
                                                 <div class="odd">
-                                                    <p class="order_number">订单号：<span>{{collection.collection_order_number}}</span></p>
-                                                    <p class="express_number">快递单号：<span>{{collection.logistics_code}}</span></p>
+                                                    <p class="order_number">订单号：
+                                                        <span>{{collection.collection_order_number}}</span>
+                                                    </p>
+                                                    <p class="express_number">快递单号：
+                                                        <span>{{collection.logistics_code}}</span>
+                                                    </p>
                                                 </div>
                                             </router-link>
                                             <div class="order-option">
-                                                <span  class="order-btn" v-if="collection.collection_order_status_id == -1">删除订单</span>
-                                                <span  class="order-btn" v-else-if="collection.collection_order_status_id == 1 || collection.collection_order_status_id == 2">取消订单</span>
-                                                <span  class="order-btn" v-else-if="collection.collection_order_status_id == 3">修改收货时间</span>
-                                                <span  class="order-btn bd-finish" v-else-if="collection.collection_order_status_id == 4">去支付</span>
-                                                <span  class="order-btn bd-finish" v-else-if="collection.collection_order_status_id == 5">去评价</span>
-                                                <span  class="order-btn" v-else-if="collection.collection_order_status_id == 6">删除订单</span>
+                                                <span class="order-btn" v-if="collection.collection_order_status_id == -1">删除订单</span>
+                                                <span class="order-btn" v-else-if="collection.collection_order_status_id == 1 || collection.collection_order_status_id == 2">取消订单</span>
+                                                <span class="order-btn" v-else-if="collection.collection_order_status_id == 3">修改收货时间</span>
+                                                <span class="order-btn bd-finish" v-else-if="collection.collection_order_status_id == 4">去支付</span>
+                                                <span class="order-btn bd-finish" v-else-if="collection.collection_order_status_id == 5">去评价</span>
+                                                <span class="order-btn" v-else-if="collection.collection_order_status_id == 6">删除订单</span>
                                             </div>
                                         </li>
                                     </div>
@@ -92,79 +98,85 @@
     </div>
 </template>
 <script>
-    import { Tab, TabItem } from 'vux'
-    export default{
-        data() {
-            return{
-                index: 0,
-                list: [
-                    { name: '我寄的', 'num': '3' },
-                    { name: '代收的', 'num': '5' },
-                ],
-                items:[],
-                ships:[],
-                collections:[],
-                express_order_status:'',
-                express_order_type:''
+import { Tab, TabItem } from 'vux'
+export default {
+    data() {
+        return {
+            index: 0,
+            list: [
+                { name: '我寄的', 'num': '0' },
+                { name: '代收的', 'num': '0' },
+            ],
+            items: [],
+            ships: [],
+            collections: [],
+            express_order_status: '',
+            express_order_type: ''
+        }
+    },
+    components: {
+        Tab,
+        TabItem
+    },
+    created() {
+        this.order(this.index)
+    },
+    methods: {
+        order(index) {
+            let that = this
+            that.express_order_status = this.$route.query.express_order_status
+            if (that.index == 0) {
+                that.express_order_type = 'ship'
+            } else {
+                that.express_order_type = 'collection'
             }
-        },
-        components: {
-            Tab,
-            TabItem
-        },
-        created() {
-            this.order(this.index)
-        },
-        methods:{
-            order(index){
-                let that = this
-                that.express_order_status = this.$route.query.express_order_status
-                if(that.index == 0){
-                    that.express_order_type = 'ship'
-                }else{
-                    that.express_order_type = 'collection'
+            this.http(that.configs.apiTop + "/order/orders?express_order_status=" + that.express_order_status + "&express_order_type=" + that.express_order_type, "get", '', function(res) {
+                let msg = res.data
+                if (msg.code == 0) {
+                    let data = msg.data
+                    that.items = data.items
+                    that.ships = data.ships
+                    that.collections = data.collections
+                    that.list[0].num = data.total_ship_order
+                    that.list[1].num = data.total_collection_order
+                } else if (msg.code == 40004) {
+                    // location.href = that.configs.accreditUrl
+                } else {
+                    that.$vux.toast.text(msg.message, 'middle', 100);
                 }
-                this.http(that.configs.apiTop + "/order/orders?express_order_status="+that.express_order_status+"&express_order_type="+that.express_order_type, "get", '', function(res) {
-                    let msg = res.data
-                    if (msg.code == 0) {
-                        let data = msg.data
-                        that.items = data.items
-                        that.ships = data.ships
-                        that.collections = data.collections
-                        that.list[0].num = data.ships.length
-                        that.list[1].num = data.collections.length
-                    } else if (msg.code == 40004) {
-                        // location.href = that.configs.accreditUrl
-                    } else {
-                        that.$vux.toast.text(msg.message, 'middle', 100);
-                    }
-                })
-            }
+            })
         }
     }
+}
 </script>
 <style lang="scss" scoped src="../../../static/assets/css/user.scss"></style>
 <style lang="scss">
-    @import '../../../static/assets/css/px2rem.scss';
+@import '../../../static/assets/css/px2rem.scss';
 
-    .vux-tab .vux-tab-item.vux-tab-selected{
-        color: #366931 !important;border-bottom:none !important;
-    }
-    .vux-tab .vux-tab-item.vux-tab-selected span{
-        border-bottom:3px solid  #366931 !important;
-        padding-bottom:px2rem(14)
-    }
-    .vux-tab-ink-bar{
-        background-color:none !important
-    }
-    .vux-tab-item + .vux-tab-ink-bar{
-        display:none !important;
-    }
-    .vux-slider > .vux-swiper{
-        min-height: -webkit-fill-available;
-    }
-    .vux-slider > .vux-swiper{
-        overflow: auto !important;
-        position: relative;
-    }
+.vux-tab .vux-tab-item.vux-tab-selected {
+    color: #366931 !important;
+    border-bottom: none !important;
+}
+
+.vux-tab .vux-tab-item.vux-tab-selected span {
+    border-bottom: 3px solid #366931 !important;
+    padding-bottom: px2rem(14)
+}
+
+.vux-tab-ink-bar {
+    background-color: none !important
+}
+
+.vux-tab-item+.vux-tab-ink-bar {
+    display: none !important;
+}
+
+.vux-slider>.vux-swiper {
+    min-height: -webkit-fill-available;
+}
+
+.vux-slider>.vux-swiper {
+    overflow: auto !important;
+    position: relative;
+}
 </style>
